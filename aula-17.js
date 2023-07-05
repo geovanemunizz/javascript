5f80d7dab1662fb300847a6f19da90eaf1e045c4
Geovane Muniz <geovanemuniz959@gmail.com>
2023-07-05 15:19:33 -0300

    update

---
+++ aula-17.js
@@ -0,0 +1,26 @@
+//loop while (enquanto)
+/* sintaxe: 
+while (condicao) {
+   bloco de comando
+}
+*/
+
+let obj = 0;
+
+while (obj <= 2) {
+  console.log(obj);
+  obj++;
+}
+
+//fatorial
+
+let n = 10;
+let fatorial = 1;
+
+while (n >= 1) {
+  fatorial *= n; 
+  n--;
+}
+
+console.log(fatorial);
+
---
+++ aula-19.js
@@ -0,0 +1,36 @@
+// break e continue
+
+let value = 0;
+let steel = 1000;
+let parimpar = 0;
+
+// while (value < steel) {
+//   console.log(" javascript - " + value);
+//   if (value >= 5) {
+//     break;
+//   }
+//   value++;
+// }
+
+for (let i = value; i < steel; i++) {
+  if (i % 2 != 0) {
+    continue;
+  }
+  parimpar++;
+}
+console.log(" numero de pares " + parimpar);
+console.log("Fim!");
+
+for (let n = 0; n < 10; n++) {
+  if (n === 5) {
+    break; //interrompe imediatamente a execução do loop e continua o programa
+  }
+  console.log(" numero - " + n);
+}
+
+for (let cont = 0; cont < 10; cont++) {
+  if (cont === 0) {
+    continue; //é usado para pular uma iteração específica e continuar com o restante do loop.
+  }
+  console.log("continue - " + cont);
+}
